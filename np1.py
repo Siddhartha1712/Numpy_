f1be2261b0c60e12582b60e3b56ede66c065fd14
Siddhartha1712 <siddharthah1712@gmail.com>
2025-06-21 01:08:52 +0530

    Adding np1.py

---
+++ np1.py
@@ -0,0 +1,40 @@
+import numpy as np
+#Different ways of creating numpy Array
+
+#np.array
+arr1 = np.array([1,2,3,1,2,3])
+print(arr1)
+
+#np.formiter
+arr2 = np.fromiter("HelloWorld",dtype='U2')
+print(arr2)
+
+#np.arange
+arr3 = np.arange(10,50,5,dtype=float)
+print(arr3)
+
+
+#np.linspace
+arr4 = np.linspace(3.5,20,5,dtype=np.int32)
+print(arr4)
+
+#np.empty
+arr5 = np.empty([3,3],dtype=np.int32)
+print(arr5)
+
+#np.ones
+arr6 = np.ones([4,4],dtype=np.float32)
+print(arr6)
+
+#np.zeros
+arr7 = np.zeros([6,6],dtype=np.int32)
+print(arr7)
+
+print("\n__________________________________________________\n")
+
+#Attributes
+print("Shape: ",arr6.shape)
+print("Dimesion: ",arr6.ndim)
+print("Size: ",arr6.size)
+print("Data Type: ",arr6.dtype)
+print("Item Size: ",arr6.itemsize)
