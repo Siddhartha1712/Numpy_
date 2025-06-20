6cf9938b8ff76c9d2472257953a66ac78fbf8034
Siddhartha1712 <its.trashi.17@gmail.com>
2025-06-20 20:39:07 +0530

    adding basic_op.py file

---
+++ basic_op.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+#Addition and subtraction on array 
+a = np.array([[1,2],[2,1]])
+
+print("Array a: \n",a)
+print("Array a+1:\n",a+1)
+print("Attay a-1:\n",a-1)
+
+#adding two array
+b = np.array([[2,1],[1,2]])
+print("Array b:\n",b)
+print("Array a+b:\n",a+b)
+print("Array a+b:\n",a-b)
+print("\n__________________________________________________\n")
+
+#Data type 
+
+a = np.array([[1,2,3],[4,5,6]])
+print("Array a:\n",a)
+print("Arrat a DataType: ",a.dtype)
+
+b = np.array([[1.0,2.0,3.0],[4.0,5.0,6.0]])
+print("Array b:\n",b)
+print("Arrat a DataType: ",b.dtype)
+
+c = np.array([["a","a","a"],["a","a","a"]])
+print("Array c:\n",c)
+print("Array a DataType: ",c.dtype)
+
+#addition of two array
+print("array sum of a+b:\n",np.add(a,b))
+#square root of an array
+print("Sqrt of a:\n",np.sqrt(a))
+#sum of array
+print("Sum of array a:",np.sum(a))
+#Transpose of array 
+print("Transpose of array a:\n",a.T)
